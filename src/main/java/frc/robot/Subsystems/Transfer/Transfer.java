67201665ddd70aedd013845ab3733f01bb23e9f2
ilaybaruch <166049550+ilaybaruch@users.noreply.github.com>
2025-10-21 08:18:47 +0300

    deleted transfer files

--- src/main/java/frc/robot/Subsystems/Transfer/Transfer.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.Subsystems.Transfer;
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class Transfer extends SubsystemBase {
-
-}
--- src/main/java/frc/robot/Subsystems/Transfer/TransferConstants.java
+++
@@ -1,6 +0,0 @@
-package frc.robot.Subsystems.Transfer;
-
-public class TransferConstants {
-    public static final int MOTOR_ID = 0;
-    public static final int LIMIT_SWITCH_CHANNEL = 0;
-}
