e7273081ae00f5ddc134b813e9232c521eb4f047
ilaybaruch <166049550+ilaybaruch@users.noreply.github.com>
2025-10-21 20:32:25 +0300

    started working on IO

--- src/main/java/frc/robot/Subsystem/Elevator/ElevatorIO.java
+++ src/main/java/frc/robot/Subsystem/Elevator/ElevatorIO.java
@@ -1,5 +1,19 @@
 package frc.robot.Subsystem.Elevator;
 
-public class ElevatorIO {
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ElevatorIO {
+
+   @AutoLog
+   
+   public static class ElevatorInputs{
+    public double voltage;
+    public double output;
+    public boolean isSwitchPressed;
+    public boolean atGoal;
+   }
+
+   public default void updateInputs(ElevatorInputs inputs){}
+
     
 }
