739a82b7952e10ae375945d4d3daeb41090de264
ilaybaruch <166049550+ilaybaruch@users.noreply.github.com>
2025-10-23 13:46:44 +0300

    added constants

--- src/main/java/frc/robot/Subsystems/Elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/Subsystems/Elevator/ElevatorConstants.java
@@ -1,5 +1,25 @@
 package frc.robot.Subsystems.Elevator;
 
 public class ElevatorConstants {
-
+    public static final int MOTOR_ID = 16;
+    public static final double Kp = 0.0;
+    public static final double Ki = 0.0;
+    public static final double Kd = 0.0;
+    public static final double Ks = 0.0;
+    public static final double Kg = 0.0;
+    public static final double Kv = 0.0;
+    public static final double Ka = 0.0;
+    public static final double MOTOR_MAX_VOLTAGE = 12;
+    public static final double MOTOR_MIN_VOLTAGE = 0;
+    public static final int CURRENT_LIMIT = 40;
+    public static final double VOLTAGE_COMPENSATION = 12;
+    public static final double POSITION_CONVERSION_FACTOR = 0;
+    public static final boolean INVERTED = true;// idk
+    public static final double MAX_ACCELERATION = 10;
+    public static final double MAX_VELOCITY = 10;
+    public static final double TOLERANCE = 0.2;
+    public static final double INTAKE_POS = 0.0;
+    public static final double L1_POS = 0.0;
+    public static final double L2_POS = 0.0;
+    public static final double MAX_POS = 0.0;
 }
