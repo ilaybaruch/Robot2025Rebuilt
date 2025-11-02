7b12c5c9d46fcff8ea61e2285e4c7a5f6b652119
ilaybaruch <166049550+ilaybaruch@users.noreply.github.com>
2025-11-02 20:37:44 +0200

    retuned

--- src/main/java/frc/robot/Subsystems/Arm/ArmConstants.java
+++ src/main/java/frc/robot/Subsystems/Arm/ArmConstants.java
@@ -9,11 +9,11 @@ public class ArmConstants {
     public static final int DOWN_LIMIT_SWITCH_CHANNEL = 2;
     public static final boolean UP_SWITCH_NORMALLY_OPEN = true;// need to check
     public static final boolean DOWN_SWITCH_NORMALLY_OPEN = true;// need to check
-    public static final double Kp = 2.9; // idk amen
+    public static final double Kp = 3.3; // idk amen
     public static final double Ki = 0.0;
     public static final double Kd = 0.0;
     public static final double Ks = 0.0;
-    public static final double Kg = 0.45;
+    public static final double Kg = 0.48;
     public static final double Kv = 0.0;
     public static final double Ka = 0.0;
     public static final double MOTOR_MAX_VOLTAGE = 12;
@@ -22,9 +22,9 @@ public class ArmConstants {
     public static final double VOLTAGE_COMPENSATION = 12;
     public static final double POSITION_CONVERSION_FACTOR = 1.0 / 40 /* gears */ * (2 * Math.PI)/* rads */;
     public static final boolean INVERTED = true; // need to check if true
-    public static final double MAX_ACCELERATION = 20;
-    public static final double MAX_VELOCITY = 3.5;
-    public static final double TOLERANCE = Units.degreesToRadians(3.0);
+    public static final double MAX_ACCELERATION = 16;
+    public static final double MAX_VELOCITY = 3;
+    public static final double TOLERANCE = Units.degreesToRadians(4);
     public static final double L1_POS = 0;
     public static final double L2_POS = 0;
     public static final double L3_POS = 0;
