42008d4117468254a91758735fad414e289adeff
ilaybaruch <166049550+ilaybaruch@users.noreply.github.com>
2025-11-03 18:11:03 +0200

    added led class

---
+++ src/main/java/frc/robot/Subsystems/LED/LedIO.java
@@ -0,0 +1,17 @@
+package frc.robot.Subsystems.LED;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface LedIO {
+
+    @AutoLog
+
+    public static class Led {
+
+    }
+
+    public default void updateInputs() {
+    }
+
+    
+}
