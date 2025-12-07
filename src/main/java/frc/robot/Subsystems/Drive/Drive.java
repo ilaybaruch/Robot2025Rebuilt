74c3a630421c2e249a836a904e46364846e60262
ilaybaruch <166049550+ilaybaruch@users.noreply.github.com>
2025-12-07 23:05:30 +0200

    working on drive

--- src/main/java/frc/robot/Subsystems/Drive/Drive.java
+++ src/main/java/frc/robot/Subsystems/Drive/Drive.java
@@ -1,3 +1,59 @@
-// public class Drive {
+package frc.robot.Subsystems.Drive;
 
-// }
+import static frc.robot.Subsystems.Drive.DriveConstants.moduleTranslations;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+
+public class Drive extends SubsystemBase {
+    boolean goodVison = true;
+    static final Lock odometryLock = new ReentrantLock();
+    private final GyroIO gyroIO;
+    private final GyroIOInputsAutoLogged gyroInputs = new GyroIOInputsAutoLogged();
+    private final Module[] modules = new Module[4]; // FL, FR, BL, BR
+
+    // public static double ODOMETRY_FREQUENCY = 0.0;
+    private SwerveDriveKinematics kinematics = new SwerveDriveKinematics(moduleTranslations);
+    private Rotation2d rawGyroRotation = new Rotation2d();
+    private SwerveModulePosition[] lastModulePositions = // For delta tracking
+            new SwerveModulePosition[] {
+                    new SwerveModulePosition(),
+                    new SwerveModulePosition(),
+                    new SwerveModulePosition(),
+                    new SwerveModulePosition()
+            };
+
+    public Drive(GyroIO gyroIO,
+            ModuleIO flModuleIO,
+            ModuleIO frModuleIO,
+            ModuleIO blModuleIO,
+            ModuleIO brModuleIO) {
+        this.gyroIO = gyroIO;
+        modules[0] = new Module(flModuleIO);
+        modules[1] = new Module(frModuleIO);
+        modules[2] = new Module(blModuleIO);
+        modules[3] = new Module(brModuleIO);
+    }
+
+    @Override
+    public void periodic() {
+        odometryLock.lock(); // Prevents odometry updates while reading data
+        gyroIO.updateInputs(gyroInputs);
+        Logger.processInputs("Drive/Gyro", gyroInputs);
+        for (var module : modules) {
+            module.periodic();
+        }
+        odometryLock.unlock();
+    }
+
+}
